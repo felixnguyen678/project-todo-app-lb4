6d799b86dd9e4a5312751be32cb34d9cd3ec76bf
nguyenquangphuc-vn <nguyenquangphuc.vn.hue@gmail.com>
2022-03-02 15:14:30 +0700

    quangphuc | fix project and task relation

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,2 @@
 export * from './ping.controller';
 export * from './project-task.controller';
-export * from './task.controller';
-export * from './project.controller';
-export * from './user.controller';
