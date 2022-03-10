290c7e6e9d26770bef2cf0340b0a8fa76a5974a8
nguyenquangphuc-vn <nguyenquangphuc.vn.hue@gmail.com>
2022-03-10 20:06:53 +0700

    feat: add user project task controllers

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -10,3 +10,6 @@ export * from './project-user-project.controller';
 export * from './project-project-user.controller';
 export * from './project-user-user.controller';
 export * from './user-project-user.controller';
+export * from './task.controller';
+export * from './user.controller';
+export * from './project.controller';
