749902c118d4e03d1a83bcbf13b19496ee9a3927
nguyenquangphuc-vn <nguyenquangphuc.vn.hue@gmail.com>
2022-03-10 18:05:02 +0700

    feat: add repositories

---
+++ src/repositories/task.repository.ts
@@ -0,0 +1,22 @@
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, BelongsToAccessor} from '@loopback/repository';
+import {MongoDataSource} from '../datasources';
+import {Task, TaskRelations, User} from '../models';
+import {UserRepository} from './user.repository';
+
+export class TaskRepository extends DefaultCrudRepository<
+  Task,
+  typeof Task.prototype.id,
+  TaskRelations
+> {
+
+  public readonly hasOwner: BelongsToAccessor<User, typeof Task.prototype.id>;
+
+  constructor(
+    @inject('datasources.mongo') dataSource: MongoDataSource, @repository.getter('UserRepository') protected userRepositoryGetter: Getter<UserRepository>,
+  ) {
+    super(Task, dataSource);
+    this.hasOwner = this.createBelongsToAccessorFor('hasOwner', userRepositoryGetter,);
+    this.registerInclusionResolver('hasOwner', this.hasOwner.inclusionResolver);
+  }
+}
