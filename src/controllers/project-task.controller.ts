991f987bdc20aa3fb67cc4a439b72b026ac4d8f7
nguyenquangphuc-vn <nguyenquangphuc.vn.hue@gmail.com>
2022-03-11 13:58:42 +0700

    feat: remove unused controllers

--- src/controllers/project-task.controller.ts
+++ src/controllers/project-task.controller.ts
@@ -45,66 +45,66 @@ export class ProjectTaskController {
     return this.projectRepository.tasks(id).find(filter);
   }
 
-  @post('/projects/{id}/tasks', {
-    responses: {
-      '200': {
-        description: 'Project model instance',
-        content: {'application/json': {schema: getModelSchemaRef(Task)}},
-      },
-    },
-  })
-  async create(
-    @param.path.string('id') id: typeof Project.prototype.id,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Task, {
-            title: 'NewTaskInProject',
-            exclude: ['id'],
-            optional: ['projectId']
-          }),
-        },
-      },
-    }) task: Omit<Task, 'id'>,
-  ): Promise<Task> {
-    return this.projectRepository.tasks(id).create(task);
-  }
-
-  @patch('/projects/{id}/tasks', {
-    responses: {
-      '200': {
-        description: 'Project.Task PATCH success count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
-  })
-  async patch(
-    @param.path.string('id') id: string,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Task, {partial: true}),
-        },
-      },
-    })
-    task: Partial<Task>,
-    @param.query.object('where', getWhereSchemaFor(Task)) where?: Where<Task>,
-  ): Promise<Count> {
-    return this.projectRepository.tasks(id).patch(task, where);
-  }
-
-  @del('/projects/{id}/tasks', {
-    responses: {
-      '200': {
-        description: 'Project.Task DELETE success count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
-  })
-  async delete(
-    @param.path.string('id') id: string,
-    @param.query.object('where', getWhereSchemaFor(Task)) where?: Where<Task>,
-  ): Promise<Count> {
-    return this.projectRepository.tasks(id).delete(where);
-  }
+  // @post('/projects/{id}/tasks', {
+  //   responses: {
+  //     '200': {
+  //       description: 'Project model instance',
+  //       content: {'application/json': {schema: getModelSchemaRef(Task)}},
+  //     },
+  //   },
+  // })
+  // async create(
+  //   @param.path.string('id') id: typeof Project.prototype.id,
+  //   @requestBody({
+  //     content: {
+  //       'application/json': {
+  //         schema: getModelSchemaRef(Task, {
+  //           title: 'NewTaskInProject',
+  //           exclude: ['id'],
+  //           optional: ['projectId']
+  //         }),
+  //       },
+  //     },
+  //   }) task: Omit<Task, 'id'>,
+  // ): Promise<Task> {
+  //   return this.projectRepository.tasks(id).create(task);
+  // }
+  //
+  // @patch('/projects/{id}/tasks', {
+  //   responses: {
+  //     '200': {
+  //       description: 'Project.Task PATCH success count',
+  //       content: {'application/json': {schema: CountSchema}},
+  //     },
+  //   },
+  // })
+  // async patch(
+  //   @param.path.string('id') id: string,
+  //   @requestBody({
+  //     content: {
+  //       'application/json': {
+  //         schema: getModelSchemaRef(Task, {partial: true}),
+  //       },
+  //     },
+  //   })
+  //   task: Partial<Task>,
+  //   @param.query.object('where', getWhereSchemaFor(Task)) where?: Where<Task>,
+  // ): Promise<Count> {
+  //   return this.projectRepository.tasks(id).patch(task, where);
+  // }
+  //
+  // @del('/projects/{id}/tasks', {
+  //   responses: {
+  //     '200': {
+  //       description: 'Project.Task DELETE success count',
+  //       content: {'application/json': {schema: CountSchema}},
+  //     },
+  //   },
+  // })
+  // async delete(
+  //   @param.path.string('id') id: string,
+  //   @param.query.object('where', getWhereSchemaFor(Task)) where?: Where<Task>,
+  // ): Promise<Count> {
+  //   return this.projectRepository.tasks(id).delete(where);
+  // }
 }
